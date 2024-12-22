16922abe3b0182e274eb62d974e41d70ee64a205
Nguemoue <luc.nguemoue@kamix.io>
2024-12-22 22:25:52 +0100

    It seems like there are changes made to three files in the repository: `resources/views/frontend/home.blade.php`, `resources/views/frontend/contact.blade.php`, and `config/projects.php`. Based on these changes, a suitable commit message in the conventional commit convention could be:
    ```
    feat(frontend): Update home and contact pages and add new info in projects config (main)
    ```
    
    This message indicates that a new feature (`feat`) has been added to the frontend section of the project (`frontend`). The changes include updating the home and contact pages, as well as adding new information to the projects configuration file. The branch name `main` is used to indicate the issue number, but since the issue number is not explicitly mentioned in the provided information, it's assumed that the changes are related to the main branch.

--- resources/views/components/frontend/home-objectives.blade.php
+++ resources/views/components/frontend/home-objectives.blade.php
@@ -1,8 +1,10 @@
 <div class="row">
-    @foreach($objectives as $objective)
-        <div class="col-sm-12 col-xs-12 col-md-6 col-xl-6 col-lg-6 col-xxl-6">
-            {{str($objective->content)->toHtmlString()}}
-        </div>
-    @endforeach
+    <ul style="list-style-type: upper-alpha">
+        @foreach($objectives as $objective)
+            <div class="col-sm-12 col-xs-12 col-md-6 col-xl-6 col-lg-6 col-xxl-6">
+                <li>{{str($objective->content)->toHtmlString()}}</li>
+            </div>
+        @endforeach
+    </ul>
 </div>
 

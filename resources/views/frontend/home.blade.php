21ee8c97a3326e0442c24ba3ea16d920ffc60aa9
Nguemoue <luc.nguemoue@kamix.io>
2024-12-22 22:20:27 +0100

    It seems like there are changes made to three files in the repository: `resources/views/frontend/home.blade.php`, `resources/views/frontend/contact.blade.php`, and `config/projects.php`. Based on these changes, a suitable commit message in the conventional commit convention could be:
    ```
    feat(frontend): Update home and contact pages and add new info in projects config (main)
    ```
    
    This message indicates that a new feature (`feat`) has been added to the frontend section of the project (`frontend`). The changes include updating the home and contact pages, as well as adding new information to the projects configuration file. The branch name `main` is used to indicate the issue number, but since the issue number is not explicitly mentioned in the provided information, it's assumed that the changes are related to the main branch.

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -3,58 +3,68 @@
 @section('content')
     <main>
         <section class="bg-grey bd-bottom circle">
-            <div class="container-fluid">
-                <div class="row">
-                    <div
-                        {{--                    class="col-sm-12 col-md-8 col-xl-8 col-lg-8 col-xxl-8 mx-auto"--}}
-                    >
-                        <section class="slider-section">
-                            <div class="slider-wrapper">
-                                <div id="main-slider" class="nivoSlider">
-                                    @for($i= 1; $i <= 5; $i++)
-                                        <img src="{{asset('assets/img/sld'.$i.'.png')}}" alt title="#slider-caption-{{$i}}"/>
-                                    @endfor
-                                </div>
-
+            <div class="row">
+                <div class="col-12">
+                    <section class="slider-section">
+                        <div class="slider-wrapper">
+                            <div id="main-slider" class="nivoSlider">
                                 @for($i= 1; $i <= 5; $i++)
-                                    <div id="slider-caption-{{$i}}" class="nivo-html-caption slider-caption">
-                                        <div class="display-table">
-                                            <div class="table-cell">
-                                                <div class="container">
-                                                    <div class="slider-text">
-                                                        <h1 class="wow cssanimation fadeInTop" data-wow-delay="1s"
-                                                            data-wow-duration="800ms">For description {{$i}}</h1>
-                                                    </div>
+                                    <img class="slider-img" src="{{asset('assets/img/sld'.$i.'.png')}}" alt
+                                         title="#slider-caption-{{$i}}"/>
+                                @endfor
+                            </div>
+
+                            @for($i= 1; $i <= 5; $i++)
+                                <div id="slider-caption-{{$i}}" class="nivo-html-caption slider-caption">
+                                    <div class="display-table">
+                                        <div class="table-cell">
+                                            <div class="container">
+                                                <div class="slider-text">
+                                                    {{--                                                        <h1 class="wow cssanimation fadeInTop" data-wow-delay="1s" data-wow-duration="800ms">For description {{$i}}</h1>--}}
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                @endfor
-
-
-                            </div>
-                        </section>
-                        <div class="mt-4 text-center">
-                            FINS-WINS
-                            L’Asbl Femmes Interface Nord-Sud (F.I.N.S) ou Women Interface North-South (W.I.N.S) est
-                            fondée en 2008. Elle est conçue comme un mouvement de réflexion et d’actions concrètes en
-                            faveur de la promotion de genre au développement.
-
-                            Elle ambitionne de renforcer l’autonomisation (empowerment) des femmes dans les activités
-                            socioéconomiques, politiques et culturelles.
-
-                            La philosophie générale de FINS-WINS est basée sur les principes de justice, de
-                            complémentarité entre les genres, de réciprocité dans les échanges Nord-Sud et de
-                            Co-développement harmonieux entre les peuples.
+                                </div>
+                            @endfor
                         </div>
-                        <div class="mt-4 text-center">
-                            <x-frontend.home-objectives/>
+                    </section>
+                </div>
+                <div class="container">
+                    <div class="mt-4 text-justify">
+                        FINS-WINS
+                        L’Asbl Femmes Interface Nord-Sud (F.I.N.S) ou Women Interface North-South (W.I.N.S) est
+                        fondée en 2008. Elle est conçue comme un mouvement de réflexion et d’actions concrètes en
+                        faveur de la promotion de genre au développement.
+
+                        Elle ambitionne de renforcer l’autonomisation (empowerment) des femmes dans les activités
+                        socioéconomiques, politiques et culturelles.
+
+                        La philosophie générale de FINS-WINS est basée sur les principes de justice, de
+                        complémentarité entre les genres, de réciprocité dans les échanges Nord-Sud et de
+                        Co-développement harmonieux entre les peuples.
+                    </div>
+                    <div class="">
+                        <div class="card-body">
+                            <div class="mt-4 text-justify">
+                                <h2 class="text-center">Objectif</h2>
+                                <x-frontend.home-objectives/>
+                            </div>
                         </div>
                     </div>
-
                 </div>
-
             </div>
+
         </section>
     </main>
 @endsection
+
+@push('stylesheets')
+    <style>
+        .slider-img {
+            background-color: red;
+            filter: contrast(20);
+            backdrop-filter: blur(10px);
+        }
+    </style>
+@endpush
